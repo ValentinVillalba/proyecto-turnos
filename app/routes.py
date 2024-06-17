4ecb591ab7d6547517c3890e8d5169ec7a61001c
Valentin Villalba <vavillalba@comunidad.unnoba.edu.ar>
2024-06-17 14:47:58 -0300

    Se crea sistema de validacion por tipo de usuario

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from datetime import date, time, timedelta
-from flask import jsonify, render_template, render_template, request, redirect, url_for, flash
+from functools import wraps
+from flask import jsonify, render_template, request, redirect, url_for, flash, session
 from app.models import create_connection, close_connection
 from app import app
 
@@ -13,10 +14,33 @@ def serialize_data(rows):
                 row[key] = str(value)
     return rows
 
+# Decoradores para el control de acceso
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            flash('Por favor, inicie sesión primero.')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def role_required(role):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if 'role' not in session or session['role'] != role:
+                flash('No tienes acceso a esta página.')
+                return redirect(url_for('logout'))
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
 #RUTAS
 
 # Ruta para obtener todos los datos de la tabla turnos en el front-end
 @app.route('/get_data_turnos')
+@login_required #Recordar agregar este decorador siempre a todas las rutas, excepto login y logout
+@role_required('secretaria') #Recordar agregar este decorador a las rutas que son SOLO para secretaria
 def get_data():
     connection = create_connection()
     if connection:
@@ -47,6 +71,10 @@ def login_post():
         close_connection(connection)
 
         if user:
+            # Guardar usuario en sesión
+            session['username'] = user['nombre_usuario']
+            session['role'] = user['rol']
+
             # Verificar el rol y redirigir según el rol
             if user['rol'] == 'secretaria':
                 return redirect(url_for('secretaria_dashboard'))
@@ -57,11 +85,25 @@ def login_post():
             return redirect(url_for('login'))
     else:
         return jsonify([])
+    
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('role', None)
+    return redirect(url_for('login'))
 
 @app.route('/secretaria_dashboard')
+@login_required
+@role_required('secretaria')
 def secretaria_dashboard():
     return render_template('secretaria_dashboard.html')
 
 @app.route('/cliente_dashboard')
+@login_required
 def cliente_dashboard():
     return render_template('cliente_dashboard.html')
+
+@app.route('/altaturno')
+@login_required
+def altaturno():
+    return render_template('altaturno.html')
