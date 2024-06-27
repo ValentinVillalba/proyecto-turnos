1acc6dee365092fc75e89da118db26566955d12b
CidCAS <gdmussi@comunidad.unnoba.edu.ar>
2024-06-27 17:01:21 -0300

    Se modificó f_todos_los_turnos.html y el script y se modificó ruta de facturista en routes.py Funciona todo ok

--- app/static/js/f_todos_los_turnos_script.js
+++ app/static/js/f_todos_los_turnos_script.js
@@ -1,48 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/get_data_turnos_facturista')
-    .then(response => response.json())
-    .then(data => {
-        const tbody = document.querySelector('#tabla-turnos');
-        data.forEach(turno => {
-            const row = document.createElement('tr');
+    document.getElementById('filterForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir comportamiento predeterminado del formulario
+        const obraSocial = document.getElementById('obraSocial').value;
+        const fechaDesde = document.getElementById('fechaDesde').value;
+        const fechaHasta = document.getElementById('fechaHasta').value;
 
-            const fechaCell = document.createElement('td');
-            const fechaUTC = new Date(turno.fecha + 'T00:00:00Z');
-            let day = fechaUTC.getUTCDate();
-            let month = fechaUTC.getUTCMonth() + 1;
-            let year = fechaUTC.getUTCFullYear();
-            
-            fechaCell.textContent = `${day}/${month}/${year}`;
-            row.appendChild(fechaCell);
+        fetch(`/get_data_turnos_facturista?obraSocial=${obraSocial}&fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`)
+            .then(response => response.json())
+            .then(data => {
+                const tbody = document.querySelector('#tabla-turnos');
+                tbody.innerHTML = ''; // Limpiar la tabla antes de llenarla con nuevos datos
+                data.forEach(turno => {
+                    const row = document.createElement('tr');
 
-            const horaCell = document.createElement('td');
-            horaCell.textContent = turno.hora;
-            row.appendChild(horaCell);
+                    const fechaCell = document.createElement('td');
+                    const fechaUTC = new Date(turno.fecha + 'T00:00:00Z');
+                    let day = fechaUTC.getUTCDate();
+                    let month = fechaUTC.getUTCMonth() + 1;
+                    let year = fechaUTC.getUTCFullYear();
 
-            const pacienteCell = document.createElement('td');
-            pacienteCell.textContent = turno.dni;
-            row.appendChild(pacienteCell);
+                    fechaCell.textContent = `${day}/${month}/${year}`;
+                    row.appendChild(fechaCell);
 
-            const nombreCell = document.createElement('td');
-            nombreCell.textContent = turno.nombre;
-            nombreCell.classList.add('nowrap');  // Añadir la clase nowrap aquí
-            row.appendChild(nombreCell);
+                    const horaCell = document.createElement('td');
+                    horaCell.textContent = turno.hora;
+                    row.appendChild(horaCell);
 
-            const obrasocialCell = document.createElement('td');
-            obrasocialCell.textContent = turno.obra_soc;
-            obrasocialCell.classList.add('nowrap');  // Añadir la clase nowrap aquí
-            row.appendChild(obrasocialCell);
+                    const pacienteCell = document.createElement('td');
+                    pacienteCell.textContent = turno.dni;
+                    row.appendChild(pacienteCell);
 
-            const estadoCell = document.createElement('td');
-            if(turno.estado == 1 && new Date(turno.fecha) < new Date()){
-                estadoCell.textContent = "Vencido";
-            }
-            else{
-                estadoCell.textContent = turno.estado == 1 ? "Pendiente" : "Cancelado";
-            }
-            row.appendChild(estadoCell);
+                    const nombreCell = document.createElement('td');
+                    nombreCell.textContent = turno.nombre;
+                    nombreCell.classList.add('nowrap');
+                    row.appendChild(nombreCell);
 
-            tbody.appendChild(row);
-        });
-    })
+                    const obrasocialCell = document.createElement('td');
+                    obrasocialCell.textContent = turno.obra_soc;
+                    obrasocialCell.classList.add('nowrap');
+                    row.appendChild(obrasocialCell);
+
+                    const estadoCell = document.createElement('td');
+                    if(turno.estado == 1 && new Date(turno.fecha) < new Date()){
+                        estadoCell.textContent = "Vencido";
+                    }
+                    else{
+                        estadoCell.textContent = turno.estado == 1 ? "Pendiente" : "Cancelado";
+                    }
+                    row.appendChild(estadoCell);
+
+                    tbody.appendChild(row);
+                });
+            })
+            .catch(error => console.error('Error fetching turnos:', error));
+    });
 });
