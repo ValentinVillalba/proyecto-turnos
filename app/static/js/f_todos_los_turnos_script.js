5fef4963cbf38b9a6fe935be27554ba149370c03
CidCAS <gdmussi@comunidad.unnoba.edu.ar>
2024-06-27 19:54:39 -0300

    Se modificó f_todos_los_turnos.html y el script ahora funciona todo ok

--- app/static/js/f_todos_los_turnos_script.js
+++ app/static/js/f_todos_los_turnos_script.js
@@ -1,27 +1,15 @@
-function consultarTurnos() {
-    console.log('Botón Consultar clicado'); // Añade un log para verificar que el evento está siendo capturado
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('filterForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir comportamiento predeterminado del formulario
+        const obraSocial = document.getElementById('obraSocial').value;
+        const fechaDesde = document.getElementById('fechaDesde').value;
+        const fechaHasta = document.getElementById('fechaHasta').value;
 
-    const fechaDesdeInput = document.getElementById('fecha-desde');
-    const fechaHastaInput = document.getElementById('fecha-hasta');
-    const tableContainer = document.querySelector('.table-container');
-    const tbody = document.querySelector('#tabla-turnos');
-
-    const fechaDesde = fechaDesdeInput.value;
-    const fechaHasta = fechaHastaInput.value;
-
-    if (fechaDesde && fechaHasta) {
-        console.log('Fecha desde:', fechaDesde, 'Fecha hasta:', fechaHasta); // Log de las fechas
-
-        fetch(`/get_data_turnos_facturista?fecha_desde=${fechaDesde}&fecha_hasta=${fechaHasta}`)
-            .then(response => {
-                console.log('Fetch realizado');
-                return response.json();
-            })
+        fetch(`/get_data_turnos_facturista?obraSocial=${obraSocial}&fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`)
+            .then(response => response.json())
             .then(data => {
-                console.log('Datos recibidos:', data); // Log de los datos recibidos
-
-                tbody.innerHTML = ''; // Limpiar la tabla antes de insertar los nuevos datos
-
+                const tbody = document.querySelector('#tabla-turnos');
+                tbody.innerHTML = ''; // Limpiar la tabla antes de llenarla con nuevos datos
                 data.forEach(turno => {
                     const row = document.createElement('tr');
 
@@ -53,25 +41,17 @@ function consultarTurnos() {
                     row.appendChild(obrasocialCell);
 
                     const estadoCell = document.createElement('td');
-                    if (turno.estado == 1 && new Date(turno.fecha) < new Date()) {
+                    if(turno.estado == 1 && new Date(turno.fecha) < new Date()){
                         estadoCell.textContent = "Vencido";
-                    } else {
+                    }
+                    else{
                         estadoCell.textContent = turno.estado == 1 ? "Pendiente" : "Cancelado";
                     }
                     row.appendChild(estadoCell);
 
                     tbody.appendChild(row);
                 });
-
-                tableContainer.style.display = 'block'; // Mostrar la tabla
             })
             .catch(error => console.error('Error fetching turnos:', error));
-    } else {
-        alert('Por favor, selecciona ambas fechas.');
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM cargado'); // Verificar que el DOM está cargado
+    });
 });
-
